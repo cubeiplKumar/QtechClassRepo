d8ffe0e34a37e5a6aed973a80f0115eb11b755e8
cubeiplkumar <emailanilkumarm@gmail.com>
2024-10-28 11:06:02 +0530

    feature202 changes

--- Application.java
+++ Application.java
@@ -15,4 +15,18 @@ public class SimpleMethodExample {
         // Printing the result
         System.out.println("The sum is: " + result);
     }
+	#feature202 changes by Ethan for  Reports Module
+	public class DemoClass{
+ 
+  public static void sampleMethod(int a,int b) throws ArithmeticException{
+      System.out.println("Hello, this is sample method");
+      int c = a/b;
+      System.out.println("c:"+c);
+ }
+  
+ public static void main (String[] args){
+    DemoClass.sampleMethod(4,2);     
+ }
+ 
+}
 }
